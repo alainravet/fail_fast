600dccd9c9968f2ac880be83af82aba876bb58ff
Alain Ravet <alain.ravet+git@gmail.com>
2010-06-24 00:42:17 +0200

    move parts to /extensions/* and  autoload them with Dir.glob

--- lib/fail_fast.rb
+++ lib/fail_fast.rb
@@ -1,12 +1,7 @@
 require 'fail_fast/main'
 
-require 'fail_fast/check_value'
-require 'fail_fast/check_file_system'
-require 'fail_fast/check_mongo_db'
-require 'fail_fast/check_active_record_db'
-require 'fail_fast/check_url'
-require 'fail_fast/check_email'
-require 'fail_fast/misc'
+# load all the extensions
+Dir.glob(File.dirname(__FILE__) + '/fail_fast/extensions/*.rb') {|file| require file }
 
 
 def FailFast(path, prefix=nil)
