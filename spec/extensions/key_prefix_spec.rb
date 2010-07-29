alainravet/fail_fast

commit b7e0b143cff151ad36e06be1a9d1d8d1df4fccf0

all the checkers now accept a custom :message => ..  -  closes gh-5

lib/fail_fast/extensions/check_active_record_db.rb
@@ -7,6 +7,7 @@ class FailFast
     #
     # Usage :
     #   has_active_record_db :host => 'localhost', :adapter => 'sqlite3', :database=> 'prod_db'
+    #   has_active_record_db :host => 'localhost', :adapter => 'sqlite3', :database=> 'prod_db', :message => 'custom message'
     #
     def has_active_record_db(*params)
       options = params.last.is_a?(Hash) ? params.pop : {}
@@ -19,7 +20,7 @@ class FailFast
         @error_message  = e.message
       end
       unless @success
-        add_error ErrorDetails.new(nil, :active_record_db_connection_error, @error_message)
+        add_error ErrorDetails.new(nil, :active_record_db_connection_error, @error_message, options[:message])
       end
     end
 
@@ -27,14 +28,14 @@ class FailFast
     #
     # Usage :
     #   has_active_record_db_for  'production/database'
+    #   has_active_record_db_for  'production/database', :message => 'custom message'
     #
     def has_active_record_db_for(key, *params)
-      return unless has_value_for key
-      return unless has_value_for "#{key}/adapter"
-      return unless has_value_for "#{key}/database"
-
       p = key_value_regexp_options(key, params)
       key, options = p.key, p.options
+      return unless has_value_for key              , :message => options[:message]
+      return unless has_value_for "#{key}/adapter" , :message => options[:message]
+      return unless has_value_for "#{key}/database", :message => options[:message]
 
       begin
         connection_options = p.value
@@ -46,7 +47,7 @@ class FailFast
         @error_message  = e.message
       end
       unless @success
-        add_error ErrorDetails.new(key, :active_record_db_connection_error, @error_message)
+        add_error ErrorDetails.new(key, :active_record_db_connection_error, @error_message, options[:message])
       end
     end
 

lib/fail_fast/extensions/check_email.rb
@@ -5,14 +5,14 @@ class FailFast
     #  has_email_for 'test/admin_email'
     #
     def has_email_for(key, *params)
-      return unless has_value_for key
-
       p = key_value_regexp_options(key, params)
       key, options = p.key, p.options
 
+      return unless has_value_for key, :message =>  options[:message]
+
       value = value_for_deep_key(key)
       if EmailValidator.invalid_email_address?(value)
-        add_error ErrorDetails.new(key, :not_an_email, value)
+        add_error ErrorDetails.new(key, :not_an_email, value, options[:message])
       end
     end
   end

lib/fail_fast/extensions/check_file_system.rb
@@ -5,10 +5,11 @@ class FailFast
     #
     # Usage
     #   directory_exists '/tmp'
+    #   directory_exists '/tmp', :message => 'custom message'
     #
-    def directory_exists(path, *params)
+    def directory_exists(path, options={})
       unless File.exists?(path) && File.directory?(path)
-        add_error ErrorDetails.new(nil, :directory_not_found, path)
+        add_error ErrorDetails.new(nil, :directory_not_found, path, options[:message])
       end
     end
 
@@ -16,10 +17,11 @@ class FailFast
     #
     # Usage
     #   file_exists '~/.bash_profile'
+    #   file_exists '~/.bash_profile', :message => 'custom message'
     #
-    def file_exists(path, *params)
+    def file_exists(path, options={})
       unless File.exists?(path) && File.file?(path)
-        add_error ErrorDetails.new(nil, :file_not_found, path)
+        add_error ErrorDetails.new(nil, :file_not_found, path, options[:message])
       end
     end
 
@@ -27,17 +29,17 @@ class FailFast
     #
     # Usage
     #   directory_exists_for 'foo/config'
+    #   directory_exists_for 'foo/config', :message => 'custom message'
     #
     def directory_exists_for(key, *params)
-      return unless has_value_for key
-
       p = key_value_regexp_options(key, params)
       key, options = p.key, p.options
 
-      path = value_for_deep_key(key)
+      return unless has_value_for key, :message => options[:message]
 
+      path = value_for_deep_key(key)
       unless File.exists?(path) && File.directory?(path)
-        add_error ErrorDetails.new(key, :directory_not_found, p.value)
+        add_error ErrorDetails.new(key, :directory_not_found, p.value, options[:message])
       end
     end
 
@@ -45,17 +47,17 @@ class FailFast
     #
     # Usage
     #   file_exists_for 'foo/config/app.yml'
+    #   file_exists_for 'foo/config/app.yml', :message => 'custom message'
     #
     def file_exists_for(key, *params)
-      return unless has_value_for key
-
       p = key_value_regexp_options(key, params)
       key, options = p.key, p.options
 
-      path = value_for_deep_key(key)
+      return unless has_value_for key, :message => options[:message]
 
+      path = value_for_deep_key(key)
       unless File.exists?(path) && File.file?(path)
-        add_error ErrorDetails.new(key, :file_not_found, p.value)
+        add_error ErrorDetails.new(key, :file_not_found, p.value, options[:message])
       end
     end
 

lib/fail_fast/extensions/check_url.rb
@@ -11,18 +11,17 @@ class FailFast
     #     has_url_for 'test/server_url', :reachable => true, :may_add_trailing_slash => true
     #
     def has_url_for(key, *params)
-      return unless has_value_for key
-
       p = key_value_regexp_options(key, params)
       key, options = p.key, p.options
+      return unless has_value_for key, :message =>  options[:message]
 
       value = value_for_deep_key(key)
       if UrlValidator.invalid_url?(value)
-        add_error ErrorDetails.new(key, :not_a_url, value)
+        add_error ErrorDetails.new(key, :not_a_url, value, options[:message])
         return
       end
       if true==options.delete(:reachable) && UrlValidator.unreachable_url?(value, options)
-        add_error ErrorDetails.new(key, :url_not_reachable, value)
+        add_error ErrorDetails.new(key, :url_not_reachable, value, options[:message])
       end
     end
   end

spec/extensions/check_url_spec.rb
@@ -19,4 +19,14 @@ before(:all) do
   it_should_raise_an_error('test/url_not_reachable', :url_not_reachable ,'when the url is not reachable') {
     has_url_for 'test/url_not_reachable', :reachable => true
   }
+
+  it "should accept a custom message for the 3 cases" do
+    FailFast(SIMPLE_FILE_PATH).check_now.but_fail_later do
+      has_url_for 'test/email',                                   :message => 'a_custom_message'
+      has_url_for 'inconnu',                                      :message => 'a_custom_message_2'
+      has_url_for 'test/url_not_reachable', :reachable => true,   :message => 'a_custom_message_3'
+    end
+    messages = FailFast.errors_db.errors_for(FailFast.errors_db.keys.first).collect { |e| e.message }
+    messages.should =~ %w(a_custom_message a_custom_message_2 a_custom_message_3)
+  end
 end

lib/fail_fast/extensions/check_is_on_path.rb
@@ -5,10 +5,11 @@ class FailFast
     #
     # Usage
     #   is_on_path 'jruby'
+    #   is_on_path 'jruby', :message => 'custom message'
     #
-    def is_on_path(app, *params)
+    def is_on_path(app, options = {})
       unless found_on_the_path(app)
-        add_error ErrorDetails.new(nil, :not_on_path, app)
+        add_error ErrorDetails.new(nil, :not_on_path, app, options[:message])
       end
     end
 
@@ -16,17 +17,14 @@ class FailFast
     #
     # Usage
     #   is_on_path_for 'file_compressor'
+    #   is_on_path_for 'file_compressor', :message => 'custom message'
     #
-    def is_on_path_for(key, *params)
-      return unless has_value_for key
-
-      p = key_value_regexp_options(key, params)
-
-      key = p.key
+    def is_on_path_for(key, options = {})
+      return unless has_value_for key, :message =>  options[:message]
       app   = value_for_deep_key(key)
 
       unless found_on_the_path(app)
-        add_error ErrorDetails.new(key, :not_on_path, app)
+        add_error ErrorDetails.new(key, :not_on_path, app, options[:message])
       end
     end
 

lib/fail_fast/extensions/check_mongo_db.rb
@@ -5,8 +5,9 @@ class FailFast
     #
     # Usage :
     #  has_mongoDB 'localhost'
+    #  has_mongoDB 'localhost', :message => 'custom message'
     #  has_mongoDB 'localhost', 'my_db', :port => 1234, :timeout => 2
-    #  has_mongoDB 'localhost', :port => 1234, :timeout => 2
+    #  has_mongoDB 'localhost', :port => 1234, :timeout => 2, :message => 'custom message'
     #
     def has_mongoDB(host, *params)
       options = params.last.is_a?(Hash) ? params.pop : {}
@@ -16,12 +17,12 @@ class FailFast
         port = options.delete(:port)
         @conn = Mongo::Connection.new(host, port, options)
       rescue Mongo::ConnectionFailure
-        add_error ErrorDetails.new(nil, :mongoDB_server_not_found, host)
+        add_error ErrorDetails.new(nil, :mongoDB_server_not_found, host, options[:message])
         return
       end
 
       if db && !@conn.database_names.include?(db)
-        add_error ErrorDetails.new(nil, :mongoDB_db_not_found, db)
+        add_error ErrorDetails.new(nil, :mongoDB_db_not_found, db, options[:message])
       end
     end
 
@@ -32,12 +33,12 @@ class FailFast
     #  has_mongoDB_for 'test/unknown_mongoDB_db', :check_database => false
     #
     def has_mongoDB_for(key, *params)
-      return unless has_value_for key
-      return unless has_value_for "#{key}/host"
-      return unless has_value_for "#{key}/database"
-
       p = key_value_regexp_options(key, params)
       key, options = p.key, p.options
+      return unless has_value_for key              , :message => options[:message]
+      return unless has_value_for "#{key}/host"    , :message => options[:message]
+      return unless has_value_for "#{key}/database", :message => options[:message]
+
 
       value = value_for_deep_key(key)
       host, port, db = value['host'], value['port'], value['database']
@@ -45,13 +46,13 @@ class FailFast
       begin
         @conn = Mongo::Connection.new(host, port)
       rescue Mongo::ConnectionFailure
-        add_error ErrorDetails.new(key, :mongoDB_server_not_found, host)
+        add_error ErrorDetails.new(key, :mongoDB_server_not_found, host, options[:message])
         return
       end
 
       must_check_db = !(false == options[:check_database])
       if must_check_db && !@conn.database_names.include?(db)
-        add_error ErrorDetails.new(key, :mongoDB_db_not_found, db)
+        add_error ErrorDetails.new(key, :mongoDB_db_not_found, db, options[:message])
       end
     end
 

spec/extensions/check_active_record_db_spec.rb
@@ -36,4 +36,18 @@ describe 'has_ar_db' do
       has_active_record_db_for 'invalid_key'
     }
   end
+
+  it "should accept a custom message for those 3 cases" do
+    exception = StandardError.new('an-error-message')
+    ActiveRecord::Base.should_receive(:establish_connection).twice.and_raise(exception)
+
+    FailFast(SIMPLE_FILE_PATH).check_now.but_fail_later do
+      has_active_record_db_for 'invalid_key',  :message => 'a_custom_message'
+      has_active_record_db_for 'db_connection',  :message => 'a_custom_message_2'
+      has_active_record_db valid_connection_options = {},  :message => 'a_custom_message_3'
+    end
+    messages = FailFast.errors_db.errors_for(FailFast.errors_db.keys.first).collect { |e| e.message }
+    messages.should =~ %w(a_custom_message a_custom_message_2 a_custom_message_3)
+  end
+
 end

commit df640fdef45ddf513d9b50bcde16f01709d3b5b2

fix : key prefixes are no longer doubled (broken by b7e0b143)

lib/fail_fast/extensions/check_active_record_db.rb
@@ -30,12 +30,12 @@ class FailFast
     #   has_active_record_db_for  'production/database'
     #   has_active_record_db_for  'production/database', :message => 'custom message'
     #
-    def has_active_record_db_for(key, *params)
-      p = key_value_regexp_options(key, params)
+    def has_active_record_db_for(raw_key, *params)
+      p = key_value_regexp_options(raw_key, params)
       key, options = p.key, p.options
-      return unless has_value_for key              , :message => options[:message]
-      return unless has_value_for "#{key}/adapter" , :message => options[:message]
-      return unless has_value_for "#{key}/database", :message => options[:message]
+      return unless has_value_for raw_key              , :message => options[:message]
+      return unless has_value_for "#{raw_key}/adapter" , :message => options[:message]
+      return unless has_value_for "#{raw_key}/database", :message => options[:message]
 
       begin
         connection_options = p.value

lib/fail_fast/extensions/check_email.rb
@@ -4,11 +4,11 @@ class FailFast
     # Usage
     #  has_email_for 'test/admin_email'
     #
-    def has_email_for(key, *params)
-      p = key_value_regexp_options(key, params)
+    def has_email_for(raw_key, *params)
+      p = key_value_regexp_options(raw_key, params)
       key, options = p.key, p.options
 
-      return unless has_value_for key, :message =>  options[:message]
+      return unless has_value_for raw_key, :message =>  options[:message]
 
       value = value_for_deep_key(key)
       if EmailValidator.invalid_email_address?(value)

lib/fail_fast/extensions/check_file_system.rb
@@ -31,11 +31,11 @@ class FailFast
     #   directory_exists_for 'foo/config'
     #   directory_exists_for 'foo/config', :message => 'custom message'
     #
-    def directory_exists_for(key, *params)
-      p = key_value_regexp_options(key, params)
+    def directory_exists_for(raw_key, *params)
+      p = key_value_regexp_options(raw_key, params)
       key, options = p.key, p.options
 
-      return unless has_value_for key, :message => options[:message]
+      return unless has_value_for raw_key, :message => options[:message]
 
       path = value_for_deep_key(key)
       unless File.exists?(path) && File.directory?(path)
@@ -49,11 +49,11 @@ class FailFast
     #   file_exists_for 'foo/config/app.yml'
     #   file_exists_for 'foo/config/app.yml', :message => 'custom message'
     #
-    def file_exists_for(key, *params)
-      p = key_value_regexp_options(key, params)
+    def file_exists_for(raw_key, *params)
+      p = key_value_regexp_options(raw_key, params)
       key, options = p.key, p.options
 
-      return unless has_value_for key, :message => options[:message]
+      return unless has_value_for raw_key, :message => options[:message]
 
       path = value_for_deep_key(key)
       unless File.exists?(path) && File.file?(path)

lib/fail_fast/extensions/check_url.rb
@@ -10,10 +10,10 @@ class FailFast
     #   (ex: http://example.com  + http://example.com/)
     #     has_url_for 'test/server_url', :reachable => true, :may_add_trailing_slash => true
     #
-    def has_url_for(key, *params)
-      p = key_value_regexp_options(key, params)
+    def has_url_for(raw_key, *params)
+      p = key_value_regexp_options(raw_key, params)
       key, options = p.key, p.options
-      return unless has_value_for key, :message =>  options[:message]
+      return unless has_value_for raw_key, :message =>  options[:message]
 
       value = value_for_deep_key(key)
       if UrlValidator.invalid_url?(value)

spec/extensions/check_url_spec.rb
@@ -4,8 +4,9 @@ describe 'has_url_for()' do
 before(:all) do
     FakeWeb.register_uri(:get, "http://example.com/index.html", :body => "I'm reachable!")
     FakeWeb.register_uri(:get, "http://localhost/index.html"  , :body => "I'm reachable!")
-    FakeWeb.register_uri(:get, "http://example.com"           , :body => "I'm reachable!")
-  end
+    FakeWeb.register_uri(:get, "http://localhost/index.html"  , :body => "I'm reachable!")
+    FakeWeb.register_uri(:get, "http://localhost:3200"        , :body => "I'm reachable!")
+end
 
   it_should_not_raise_an_error('when the value is an url') { has_url_for 'test/url' }
   it_should_not_raise_an_error("when the domain is 'localhost'") { has_url_for 'test/url_localhost' }

spec/extensions/key_prefix_spec.rb
@@ -1,0 +1,27 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe 'the key prefix' do
+
+  example 'works fine in all the checkers' do
+    FakeWeb.register_uri(:get, "http://localhost:3200"        , :body => "I'm reachable!")
+
+    conn = Mongo::Connection.new
+    conn.should_receive(:database_names).any_number_of_times.and_return %w(sample_mongoDB)
+    Mongo::Connection.should_receive(:new).and_return(conn)
+
+    arconn = Object.new
+    arconn.should_receive(:active?).and_return(true)
+    ActiveRecord::Base.should_receive(:connection).and_return(arconn)
+
+    FailFast(SIMPLE_FILE_PATH, 'test').check do
+      has_value_for             'email'
+      has_email_for             'email'
+      has_url_for               'url'
+      directory_exists_for      'a_directory'
+      file_exists_for           'a_file'
+      has_mongoDB               'mongoDB'
+      has_active_record_db_for  'db_connection'
+      is_on_path_for            'app_on_path'
+    end
+  end
+end

spec/fixtures/simple.yml
@@ -4,12 +4,16 @@ last_key: dernier
 app_on_path: ls
 app_not_on_path: ls0123
 test:
+  app_on_path: ls
   host: localhost
   url: http://example.com/index.html
   url_localhost: http://localhost/index.html
   email: test@example.com
   url_reachable: http://example.com
   url_not_reachable: http://xxx.zzz
+  db_connection:
+    adapter: mysql
+    database: a_db
   mongoDB:
     host: localhost
     database: sample_mongoDB
@@ -33,3 +37,6 @@ db_connection:
 number_six: 6
 letter_x: x
 nda_file: <%= SPEC_DIR + '/fixtures/simple.yml' %>
+development:
+  t_url: 'http://localhost:3200'
+  t_email: t_email@test.com