93bc19cabbbf1530d29e9d88914a513860225525
Alain Ravet <alainravet@gmail.com>
2011-06-16 23:09:02 +0200

    move FailFast code back to fail_fast.rb

--- lib/fail_fast.rb
+++ lib/fail_fast.rb
@@ -1,4 +1,38 @@
-require 'fail_fast/main'
+require 'yaml'
+require 'erb'
+
+require File.expand_path(File.dirname(__FILE__) + '/fail_fast/support/error_db')
+class FailFast
+
+  @@_errors_db = FailFast::ErrorDb.new
+
+  def initialize(config_file_path, keys_prefix=nil)
+    @config_file_path = config_file_path
+    @keys_prefix      = keys_prefix
+    @errors_key       = ErrorDb.key_for(config_file_path, keys_prefix)
+  end
+
+  def self.fail_now
+    exit(1) unless errors_db.keys.empty?
+  end
+
+  def self.failed?
+    !global_errors.empty?
+  end
+
+  def self.errors_db #:nodoc:
+    @@_errors_db
+  end
+
+  def add_error(value)
+    @@_errors_db.append(@errors_key, value)
+  end
+
+  def errors
+    @@_errors_db.errors_for(@errors_key)
+  end
+end
+
 Dir.glob(File.dirname(__FILE__) + '/fail_fast/support/*.rb'   ) {|file| require file }
 Dir.glob(File.dirname(__FILE__) + '/fail_fast/base/*.rb'      ) {|file| require file }
 Dir.glob(File.dirname(__FILE__) + '/fail_fast/extensions/*.rb') {|file| require file }
