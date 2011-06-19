79a9b43c0e1917d1d17549535c60c2d6cd723e01
Alain Ravet <alainravet@gmail.com>
2011-06-19 23:00:21 +0200

    clarify : rename and split the Hoptoad tests

--- spec/error_reporter/hoptoad/hoptoad_requests_spec.rb
+++ spec/error_reporter/hoptoad/hoptoad_requests_spec.rb
@@ -61,36 +61,4 @@ describe FailFast::ErrorReporter::Hoptoad do
     end
   end
 
-
-#--------------------------------------------------------
-  context 'when the API token is invalid' do
-    use_vcr_cassette :record => :new_episodes
-    before do
-      FailFast.init_global_error_reporters
-      @reporter = FailFast::ErrorReporter::Hoptoad.new(@api_key=INVALID_HOPTOAD_API_KEY)
-      FailFast.report_to @reporter
-    end
-
-    example 'POST => 422' do
-      produce_1_error_in_1_check_block
-      @reporter.response.code.to_i.should == 422
-    end
-  end
-
-
-  context 'when the API token is valid' do
-    use_vcr_cassette :record => :new_episodes
-
-    before do
-      FailFast.init_global_error_reporters
-      @reporter = FailFast::ErrorReporter::Hoptoad.new(@api_key=VALID_HOPTOAD_API_KEY)
-      FailFast.report_to @reporter
-    end
-
-    example 'POST => 200' do
-      produce_1_error_in_1_check_block
-      @reporter.response.code.to_i.should == 200
-    end
-  end
-
 end
