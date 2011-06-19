abc5110286f38d894ee03fa0c1e1356e883aca70
Alain Ravet <alainravet@gmail.com>
2011-06-19 22:55:43 +0200

    uniformize files distributions in spec/error_reporter

--- spec/error_reporter/stdout/stdout_spec.rb
+++ spec/error_reporter/stdout/stdout_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe FailFast::ErrorReporter::Stdout do
   before(:each) { capture_stdout }
