39e91213500dc021d2fb12d8cf4939f8eb6b715d
Alain Ravet <alainravet@gmail.com>
2011-06-21 18:45:07 +0200

    park the non-gem Hoptoad code to HoptoadRaw
    (we will add a gem-based - like for Exceptional - Hoptoad reporter next)

--- spec/error_reporters/hoptoad_raw/activating_the_reporter_spec.rb
+++ spec/error_reporters/hoptoad_raw/activating_the_reporter_spec.rb
@@ -1,12 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe FailFast::ErrorReporter::Hoptoad do
+describe FailFast::ErrorReporter::HoptoadRaw do
 
-  it 'is activated by adding it with FF#report_to to the global active error reporters' do
-    FailFast.report_to :hoptoad => VALID_HOPTOAD_API_KEY
+  it 'is activated with FailFast.report_to :hoptoad_raw => <your-hoptoad-api-key>' do
+    FailFast.report_to :hoptoad_raw => VALID_HOPTOAD_API_KEY
 
     FailFast.new.error_reporters.collect{|o|o.class}.
-        should == [FailFast::ErrorReporter::Stdout, FailFast::ErrorReporter::Hoptoad]
+        should == [FailFast::ErrorReporter::Stdout, FailFast::ErrorReporter::HoptoadRaw]
     #                                               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     #                                                 The new global errors reporter
   end
